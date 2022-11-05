d051b1bdc582845f6995f73afa4d9b12ef1958ba
Elesionore <107849917+Elesionore@users.noreply.github.com>
2022-11-05 19:23:11 +0200

    minor changes

--- src/index.js
+++ src/index.js
@@ -45,41 +45,17 @@ function formatHours(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
-
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img 
@@ -90,17 +66,16 @@ function displayForecast(response) {
             width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${
+            <span class="weather-forecast-temperature-max"> ${Math.round(
               forecastDay.temp.max
-            }º 
-            </span>
-            <span class="weather-forecast-temperature-min"> ${
+            )}º </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(
               forecastDay.temp.min
-            }º 
-            </span>
+            )}º </span>
           </div>
         </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -119,13 +94,6 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input").value;
-  document.querySelector("#city1").innerHTML = "#city-input".value;
-  searchCity(cityInput);
 }
 
 function showTemperature(response) {
@@ -148,6 +116,20 @@ function showTemperature(response) {
   getForecast(response.data.coord);
 }
 
+let dateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+dateElement.innerHTML = formatDate(currentDate);
+let timeElement = document.querySelector("#current-time");
+let currentTime = new Date();
+timeElement.innerHTML = formatHours(currentTime);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input").value;
+  document.querySelector("#city1").innerHTML = "#city-input".value;
+  searchCity(cityInput);
+}
+
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -158,20 +140,7 @@ function convertToFahrenheit(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature * 1.8 + 32);
 }
-let celsiusTemperature = null;
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
 
-let dateElement = document.querySelector("#current-date");
-let currentDate = new Date();
-dateElement.innerHTML = formatDate(currentDate);
-let timeElement = document.querySelector("#current-time");
-let currentTime = new Date();
-timeElement.innerHTML = formatHours(currentTime);
-
-searchCity("Kyiv");
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
@@ -185,6 +154,8 @@ function getCurrentPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+
+searchCity("Kyiv");
 
 function chooseParis(event) {
   event.preventDefault();
